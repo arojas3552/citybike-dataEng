434843aa486510a14fd4864ce1ba8ed7cdeb1c0f
arojas3552 <arojas@udel.edu>
2024-04-05 20:58:33 -0400

    debug: streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import google.auth
 from google.oauth2 import service_account
  
 credentials = service_account.Credentials.from_service_account_info(
-        st.secrets["gcp_service_account"],
+        st.secrets.gcp_service_account,
         scopes=["https://www.googleapis.com/auth/bigquery"])
 client = bigquery.Client(credentials=credentials)
 
