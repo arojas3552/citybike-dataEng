cc0b40fb4973c40e9cfd65d2d25a67531c219117
arojas3552 <arojas@udel.edu>
2024-04-05 21:02:16 -0400

    debug: streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from google.oauth2 import service_account
  
 credentials = service_account.Credentials.from_service_account_info(
         st.secrets.gcp_service_account,
-        scopes=["https://www.googleapis.com/auth/bigquery"])
+        )
 client = bigquery.Client(credentials=credentials)
 
 st.set_page_config(page_title="Streamlit: City Bikes", layout="wide")
